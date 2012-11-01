5f3c48723e9ad5d06d59b64c3d5e9d762e68115a
Jonas Wagner <jwagner@raptus.com>
2012-11-01 20:44:56 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b8'
 setup(name='raptus.article.gallery',
       version=version,
       description="Provides basic gallery components.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "MANUAL.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -17,7 +17,7 @@ setup(name='raptus.article.gallery',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.article/raptus.article.gallery',
+      url='https://github.com/Raptus/raptus.article.gallery',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.article'],
